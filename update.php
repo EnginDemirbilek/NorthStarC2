65e72984fa15d1324bf4516f37cc7601a709b7c1
Engin Demirbilek <engindemirbilek@protonmail.com>
2020-05-22 01:33:29 +0300

    Security enchs on second stage

--- update.php
+++ update.php
@@ -4,32 +4,44 @@ include 'chcksid.php';
 include 'functions/updateLogs.function.php';
 include 'functions/updateSessionDetails.function.php';
 include 'functions/retrieveKey.function.php';
+include 'functions/sessionExists.function.php';
 include 'ipAndUserAgent.php';
 
 // Get initial informations from client
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["opsys"]) && isset($_POST["sus"]) && isset($_POST["mName"]) && isset($_POST["wdir"]) && isset($_POST["isadm"]))
 {
-    $xorkey = retrieveXorKey($conn, $str);
-    $opsys = decrypt($_POST["opsys"], $xorkey);
-    $sus = decrypt($_POST["sus"], $xorkey);
-    $mName = decrypt($_POST["mName"], $xorkey);
-    $wdir = decrypt($_POST["wdir"], $xorkey);
-    $isAdmin = decrypt($_POST["isadm"], $xorkey);
-    $pcid = decrypt($_POST["pid"], $xorkey);
-    $pid = (int)$pcid;
-
-    //decrypt messages with key and push new infos to database
-    if (strlen($sus) > 3 && strlen($mName) > 3 && strlen($wdir) > 3 && strlen($opsys) > 3)
+    $session = sessionExists($str, $conn);
+    if ($session == "Session exists")
     {
-        $updatedDetails = updateSessionDetails($conn, $pid, $opsys, $sus, $mName, $wdir, $isAdmin, $str, $ip);
-        if (strpos($updatedDetails, 'error'))
+
+        $xorkey = retrieveXorKey($conn, $str);
+        $opsys = decrypt($_POST["opsys"], $xorkey);
+        $sus = decrypt($_POST["sus"], $xorkey);
+        $mName = decrypt($_POST["mName"], $xorkey);
+        $wdir = decrypt($_POST["wdir"], $xorkey);
+        $isAdmin = decrypt($_POST["isadm"], $xorkey);
+        $pcid = decrypt($_POST["pid"], $xorkey);
+        $pid = (int)$pcid;
+
+        //decrypt messages with key and push new infos to database
+        if (strlen($sus) > 3 && strlen($mName) > 3 && strlen($wdir) > 3 && strlen($opsys) > 3)
         {
-            updateLogs($conn, "Error", "Error; update details", $agent, $str, $ip);
+            $updatedDetails = updateSessionDetails($conn, $pid, $opsys, $sus, $mName, $wdir, $isAdmin, $str, $ip);
+            if (strpos($updatedDetails, 'error'))
+            {
+                updateLogs($conn, "Error", "Error; update details", $agent, $str, $ip);
+            }
+            elseif ($updatedDetails == "completed")
+            {
+                updateLogs($conn, "Second stage", "Registration OK", $agent, $str, $ip);
+            }
         }
-        elseif ($updatedDetails == "completed")
-        {
-            updateLogs($conn, "Second stage", "Registration OK", $agent, $str, $ip);
-        }
+
+    }
+    else
+    {
+        updateLogs($conn, "Warning", "Fake registration attempt", $agent, $str, $ip);
+        die();
     }
 
 }
@@ -42,3 +54,4 @@ else
     exit;
 }
 ?>
+
